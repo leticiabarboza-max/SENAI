02a960d09b928b7ea30d09fa0876f5c136ed904c
leticiabarboza-max <leticia_barboza@estudante.sesisenai.org.br>
2026-02-27 19:43:56 +0000

    update

---
+++ atividade 1/atividade4.py
@@ -0,0 +1,13 @@
+nome = input("insira o nome do aluno")
+nota1 = float(input("insira a primeira nota"))
+nota2 = float(input("insira a segunda nota"))
+media= (nota1 + nota2) / 2
+print(" A média das notas é:", media)
+if media >= 7:
+    print("o aluno está aprovado")
+else:
+    print("o aluno está reprovado")
+    
+    
+
+
